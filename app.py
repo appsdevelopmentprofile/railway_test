f52cea464ef5f1c7581ab2c7ce39e3d8816f27e9
J R <juanchoct96@gmail.com>
2024-11-08 16:18:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -33,22 +33,6 @@ if authentication_status == None:
     st.warning("Please enter your username and password")
 
 if authentication_status:
-    # ---- READ EXCEL ----
-    @st.cache
-    def get_data_from_excel():
-        df = pd.read_excel(
-            io="supermarkt_sales.xlsx",
-            engine="openpyxl",
-            sheet_name="Sales",
-            skiprows=3,
-            usecols="B:R",
-            nrows=1000,
-        )
-        # Add 'hour' column to dataframe
-        df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
-        return df
-
-    df = get_data_from_excel()
 
     # ---- SIDEBAR ----
     authenticator.logout("Logout", "sidebar")
