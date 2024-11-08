29b9ce06f68f7654893b55a0492ff87ea9ce6de8
J R <juanchoct96@gmail.com>
2024-11-08 13:13:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,41 @@ st.set_page_config(
 )
 
 
+
+
+
+
+
+# --- USER AUTHENTICATION ---
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+
+
+
+
+    
+
+
+
+
 # Define credentials with hashed password
 credentials = {
     "usernames": {
