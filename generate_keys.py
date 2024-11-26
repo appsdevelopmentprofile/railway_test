26d51064f78f8e31ced82fe5d3f03e2b6402cac7
J R <juanchoct96@gmail.com>
2024-11-26 15:09:38 -0700

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 import streamlit_authenticator as stauth
 
-names = ["allnorth_consultants"]
-usernames = ["rfocentral"]
+names = ["corsarious"]
+usernames = ["corsarious"]
 passwords = ["116I90ZsU\-Z"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
